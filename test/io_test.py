de14d1e81b0faf4f4d9a66049e628c8753cf6c62
yoshonabee <jhann0120@gmail.com>
2018-08-10 06:22:49 +0000

    camera_test03

--- test/io_test.py
+++ test/io_test.py
@@ -1,9 +1,7 @@
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
-GPIO.setup(3, GPIO.OUTPUT)
-GPIO.setup(5, GPIO.OUTPUT)
-
-GPIO.output(3, GPIO.HIGH)
-GPIO.output(5, GPIO.LOW)
+GPIO.setup(3, GPIO.IN)
+GPIO.setup(5, GPIO.IN)

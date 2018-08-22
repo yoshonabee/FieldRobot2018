5fe943d0f9b823b10b83e219b860624a6b4676ab
yoshonabee <jhann0120@gmail.com>
2018-08-22 23:13:03 +0800

    arduino

--- arduino/mega/mega.ino
+++ arduino/mega/mega.ino
@@ -1,18 +1,21 @@
 #include <Wire.h>
+int state = 0;
 
 void setup() {
+  pinMode(13, OUTPUT);
   Wire.begin(0x08);
   Serial.begin(115200);
-  Wire.onReceive(receiveEvent)
+  Wire.onReceive(receiveEvent);
 }
 
 void loop() {
   delay(100);
 }
 
-void receiveEvent() {
+void receiveEvent(int how) {
   while (Wire.available()) {
-    char r = Wire.read();
-    Serial.println(r);
+    state += 1;
+    int r = Wire.read();
+    digitalWrite(13, state % 2);
   }
 }
--- arduino/uno/uno.ino
+++ arduino/uno/uno.ino
@@ -1,11 +1,28 @@
 #include <Wire.h>
+int state = 0;
 
 void setup() {
+  pinMode(13, OUTPUT);
   Wire.begin(0x04);
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
 }
 
 void loop() {
-  int r = Wire.read();
-  Serial.println(r);
+  delay(100);
 }
+
+void receiveEvent(int how) {
+  while (Wire.available()) {
+    state += 1;
+    int r = Wire.read();
+    digitalWrite(13, state % 2);
+    Wire.write("1");
+  }
+}
+
+void requestEvent(int how) {
+  Wire.write("good");
+}
+

df606c61f5d55af901ee976613602cd93bb8f6c0
yoshonabee <jhann0120@gmail.com>
2018-08-26 20:18:47 +0800

    i2c_test

--- test/i2c_test.py
+++ test/i2c_test.py
@@ -1,15 +1,18 @@
 import smbus
-import time
-# for RPI version 1, use "bus = smbus.SMBus(0)"
+from time import sleep
+
 i2c = smbus.SMBus(1)
 
-# This is the address we setup in the Arduino Program
 ADDR_UNO = 0x04
 ADDR_MEGA = 0x08
 
-for i in range(100)
-	if i % 2 == 1:
-		i2c.write_byte(ADDR_UNO, i)
-	else:
-		i2c.write_byte(ADDR_MEGA, i)
-	time.sleep(0.5)
+speed = 0.015
+
+while True:
+	i2c.write_byte(ord('T'))
+	i2c.write_byte(str(len(str(speed))))
+	
+	for s in str(speed):
+		i2c.write_byte(s)
+	
+	sleep(1)

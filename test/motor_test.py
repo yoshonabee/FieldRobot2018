d5d8e19633e1120813fb671eee91c5d8b62fef40
yoshonabee <jhann0120@gmail.com>
2018-10-30 12:50:09 +0800

    turn

--- test/motor_test.py
+++ test/motor_test.py
@@ -9,7 +9,9 @@ ADDR_RIGHT = 0x08
 # speed = 45
 
 # while True:
-bus.write_i2c_block_data(ADDR_LEFT, ord('F'), [ord(s) for s in str(60)])
+bus.write_i2c_block_data(ADDR_LEFT, ord('B'), [ord(s) for s in str(60)])
 bus.write_i2c_block_data(ADDR_RIGHT, ord('F'), [ord(s) for s in str(60)])
 print(60, 60)
-sleep(5)
+sleep(1)
+bus.write_i2c_block_data(ADDR_LEFT, ord('F'), [ord(s) for s in str(0)])
+bus.write_i2c_block_data(ADDR_RIGHT, ord('F'), [ord(s) for s in str(0)])
